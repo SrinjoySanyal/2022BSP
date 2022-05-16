5655baa462750dc3a07ca901d631faa55990f39c
Srinjoy Sanyal <srinjoy.sanyal.001@student.uni.lu>
2022-05-16 19:36:26 +0200

    testing

--- auth0-express-pug-sample/src/views/QuillGitHelper.js
+++ auth0-express-pug-sample/src/views/QuillGitHelper.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Simplegit = require('simple-git');
+//simpleGit().clean(simpleGit.CleanOptions.FORCE);
 
 const git = new Simplegit();
 
@@ -9,67 +10,70 @@ function uploadDelta(version1){
   let version0 = new Array();
   fs.readFile('file.json',(err, data) => {
     if(err){
-      fs.writeFile('file.json', version1.stringify(), (err) => {console.log(err);});
+      fs.writeFile('file.json', JSON.stringify(version1), (err) => {console.log(err);});
       git.add(['file.json']).push();
     }
     else{
       let array1 = [];
       let index1 = 0;
+      version0 = JSON.parse(data);
 
       for(element in version0){
-        if(element in version1.slice(index1 + 1, version1.length() - 1)){
+        if(element in version1.slice(index1 + 1, version1.length - 1)){
         array1.push(version1.indexOf(element));
-        index1 = indexOf(element);
+        index1 = version0.indexOf(element);
         }
       }
 
-      if(version0.slice(0, array1[0]).length() == 0){
-        if(version1.slice(0, array1[0]).length() != 0){
-          version0.splice(0, version1.slice(0, array1[0]))
+      if(version0.slice(0, array1[0]).length == 0){
+        if(version1.slice(0, array1[0]).length != 0){
+          version0.splice(0, version1.slice(0, array1[0]));
         }
       }
 
-      if(version1.slice(0, array1[0]).length() == 0){
-        if(version0.slice(0, array1[0]).length() != 0){
-          version1.splice(0, version0.slice(0, array1[0]))
+      if(version1.slice(0, array1[0]).length == 0){
+        if(version0.slice(0, array1[0]).length != 0){
+          version1.splice(0, version0.slice(0, array1[0]));
         }
       }
 
-      for(i = 0; i < array1.length() - 1; i++){
-        if(version0.slice(array1[i] + 1, array1[i + 1]).length() == 0){
-          if(version1.slice(array1[i] + 1, array1[i + 1]).length() != 0){
-            version0.splice(i, version1.slice(array1[i] + 1, array1[i + 1]).length())
+      for(i = 0; i < array1.length - 1; i++){
+        if(version0.slice(array1[i] + 1, array1[i + 1]).length == 0){
+          if(version1.slice(array1[i] + 1, array1[i + 1]).length != 0){
+            version0.splice(i, version1.slice(array1[i] + 1, array1[i + 1]));
           }
         }
       }
       
-      for(j = 0; j < array1.length() - 1; j++){
-        if(version1.slice(array1[j] + 1, array1[j + 1]).length() == 0){
-          if(version0.slice(array1[j] + 1, array1[j + 1]).length() != 0){
-            version1.splice(j, version0.slice(array1[j] + 1, array1[j + 1]).length())
+      for(j = 0; j < array1.length - 1; j++){
+        if(version1.slice(array1[j] + 1, array1[j + 1]).length == 0){
+          if(version0.slice(array1[j] + 1, array1[j + 1]).length != 0){
+            version1.splice(j, version0.slice(array1[j] + 1, array1[j + 1]).length)
           }
         }
       }
 
-      fs.writeFile('file.json', version0.stringify(), (err) => {console.log(err);});
+      fs.writeFile('file.json', JSON.stringify(version0), (err) => {console.log(err);});
       git.add('file.json').push();
       git.branch('Merge-branch');
       git.checkout({'-b':'Merge-branch'});
-      fs.writeFile('file.json', version1.stringify(), (err) => {console.log(err);});
+      fs.writeFile('file.json', JSON.stringify(version1), (err) => {console.log(err);});
       git.add('file.json').push();
-      git.merge((err) => {
+      git.checkout({'-b':'master'});
+      git.mergeFromTo("https://github.com/SrinjoySanyal/subrepo.git","master",{'-m':'"merged"'}, (msg, err) => {
         if(err){
           console.log(err);
         }
         else{
           console.log('Merge successful');
         }
-      })
-      
+      });
+      git.deleteLocalBranch('Merge-branch') 
     }
   });
 }
 
-let test = '[{"ops":[{"insert":"Hello"}]},{"ops":[{"insert":"yuyu"}]},{"ops":[{"insert":"iuiu"}]}]';
+let test = '[{"ops":[{"insert":"hello"}]}]';
 
-uploadDelta(test.parse);
+uploadDelta(JSON.parse(test));
+//uploadDelta(JSON.parse(test));
